28d53a65fda634b09158587e6bdf609bc18919b5
BraedonB98 <braedonb98@gmail.com>
2024-11-24 18:03:47 -0700

    admin model changes

---
+++ models/users/admin-model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const { isEmail, isMobilePhone, isStrongPassword } = require('validator');
+
+const adminSchema = new Schema({
+  schoolAdminID: { type: String , required: true},
+  birthdate: { type: Date },
+  address: { type: String },
+  permanentAddress: { type: String},
+  universityStructure:{
+    school: { type: mongoose.Types.ObjectId, ref: "School" },
+    department: { type: mongoose.Types.ObjectId, ref: "Department" },
+    title: { type: String },
+  },
+  engagementPostings: {
+    careerFair: [{ type: mongoose.Types.ObjectId, ref: "CareerFair" }],
+    internship: [{ type: mongoose.Types.ObjectId, ref: "Internship" }],
+    event: [{ type: mongoose.Types.ObjectId, ref: "Event" }],
+    resource: [{ type: mongoose.Types.ObjectId, ref: "Resource" }],
+  },
+  analytics:{
+    accountCreated: { type: Date , default: Date.now},
+  },
+  permissions: [{ type: mongoose.Types.ObjectId, ref: "Permission" }],
+  dashboard: {
+    resources: [{ type: mongoose.Types.ObjectId, ref: "Resource" }],
+    events: [{ type: mongoose.Types.ObjectId, ref: "Event" }],
+    announcements: [{ type: mongoose.Types.ObjectId, ref: "Announcement" }],
+  },
+  preferences: {
+    notifications: { type: Boolean },//expand this later to time, email, phone, ext
+  },
+});
+
+module.exports = mongoose.model("Admin", adminSchema);
