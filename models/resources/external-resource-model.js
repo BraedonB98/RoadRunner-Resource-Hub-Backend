ebc6f554d9af5911bf201feb3fb4816ba6f11df3
BraedonB98 <braedonb98@gmail.com>
2024-12-04 19:36:02 -0700

    Moving Frontend Images into a file structure and adding comments to external resource model for future implements

--- models/resources/external-resource-model.js
+++ models/resources/external-resource-model.js
@@ -6,10 +6,12 @@ const ExternalResourceSchema = new Schema({
   title: { type: String, required: true },
   tags: [{ type: String }],
   description: { type: String, required: true },
+  //!Add priority field to schema - 1-10(the frontend will sort the resources based on this field in descending order)
   link: { type: String },
   image: { type: String, required: true },
   creator: { type: mongoose.Types.ObjectId, ref: "User", required: true },
   audience: [{ type: String }], // array to allow for multiple audiences
+  //!Add Department or Major or similar field to schema(students can filter on a page by department for example)
   users: [{ type: mongoose.Types.ObjectId, ref: "User" }],
   analytics: {
     resourceCreated: { type: Date, default: Date.now },
